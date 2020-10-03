f2a0bdeb35fad2add4f91cb0128ccf0c6c7fdde4
Phylesia <phylesiadailey@gmail.com>
2020-10-02 21:51:32 -0500

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '343553153563387',
+        clientSecret: '846599f88629d22cea818c06815dba19'
+    }
 }
